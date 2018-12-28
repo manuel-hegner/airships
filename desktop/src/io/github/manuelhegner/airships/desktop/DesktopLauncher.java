c5b744350456a4322c5075631b52edf3f3ac14e7
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2018-12-28 17:52:45 +0100

    initial project

---
+++ desktop/src/io/github/manuelhegner/airships/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.manuelhegner.airships.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.manuelhegner.airships.AirshipsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AirshipsGame(), config);
+	}
+}
