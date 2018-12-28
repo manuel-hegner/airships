495c1b45a65c9566f42be65128790be37d234172
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2018-12-28 18:11:10 +0100

    java 10

--- desktop/src/main/java/io/github/manuelhegner/airships/desktop/DesktopLauncher.java
+++ desktop/src/main/java/io/github/manuelhegner/airships/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import io.github.manuelhegner.airships.AirshipsGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 1000;
 		new LwjglApplication(new AirshipsGame(), config);
 	}
 }
